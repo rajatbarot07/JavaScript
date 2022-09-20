10788c53b4fb5b09cd43fb8db42dc3ad01f23c69
Rajat Barot <rajatbarot169@gmail.com>
2022-09-20 11:43:54 +0530

    next_prev

---
+++ Next_Prev/js/main.js
@@ -0,0 +1,25 @@
+var slider_img = document.querySelector('.slider-img');
+var images = ['image1.jfif','image2.jfif','image3.jfif','image4.jfif','image5.jfif','image6.jfif','image7.jfif'];
+var i = 0;
+
+function prev()
+{
+	if(i <= 0) 
+    i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next()
+{
+	if(i >= images.length-1) 
+	i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+ images[i]);
+	
+}
+
